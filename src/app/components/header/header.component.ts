c1aa7262a8cc32f649f30293937cc9dd46dda90a
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-19 18:00:41 +0530

    write the in header.ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  @Output() toggleSidebarForMe: EventEmitter<any> = new EventEmitter();
+  toggleSidebar() {
+    this.toggleSidebarForMe.emit();
+  }
 
 }
