153b3f327aaf496e1045534731cad28283a46390
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-19 15:27:47 +0530

    create layout component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SigninComponent } from './pages/signin/signin.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
     SignupComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
