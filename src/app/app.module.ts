70b8322f3626358d11f79e432703ef7d411ffc5c
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-19 13:55:14 +0530

    delete lofin in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './pages/login/login.component';
 import { SigninComponent } from './pages/signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     SigninComponent
   ],
   imports: [
