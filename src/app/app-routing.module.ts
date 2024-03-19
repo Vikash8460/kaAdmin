505bb9aa8f1e9b35522dbf4ce0f3137d81c73e23
Vikash Kumar <94213875+Vikash8460@users.noreply.github.com>
2024-03-19 15:19:45 +0530

    make routing to the pages in app-routing.modules.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './pages/signin/signin.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/signin', pathMatch: 'full' },
+  { path: 'signin', component: SigninComponent },
+//{ path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'signup', component: SignupComponent },
+
+  // {
+  //   path: 'admin',
+  //   // canActivate: [authGuard],
+  //   loadChildren: () =>
+  //     import('./_modules/admin/admin.module').then((m) => m.AdminModule),
+  // },
+  { path: '**', component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
